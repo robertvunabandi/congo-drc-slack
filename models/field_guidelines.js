3873047d44ff686f5c2d28ac2bb67596860f9f3f
Robert Vunabandi <rvunabandi@gmail.com>
2018-06-09 20:24:35 -0700

    updated guidelines formatting: fixed bordering lines lines

--- models/field_guidelines.js
+++ models/field_guidelines.js
@@ -1,13 +1,13 @@
 let guidelines = "\n\n";
 
-guidelines += "----------------------------------------------\n";
+guidelines += "=================================================================\n";
 guidelines += "*Expertise Input Guidelines*\n\n";
 guidelines += "- You must enter your expertise in comma separated values. ";
 guidelines += "For example, \n\n`/set_expertise computer science, sports, ice-skating`.\n\n";
 guidelines += "- Each field _must_ contain only characters from the alphabet in addition to ";
 guidelines += "hyphens (-) and empty spaces ( ). If your fields contain anything else ";
 guidelines += "(no numbers allowed), your request will not be accepted. \n\n";
-guidelines += "Please follow this guideline. _Thank you for your cooperation!_";
-guidelines += "----------------------------------------------\n";
+guidelines += "Please follow this guideline. _Thank you for your cooperation!_\n";
+guidelines += "=================================================================\n";
 
 module.exports = guidelines;
