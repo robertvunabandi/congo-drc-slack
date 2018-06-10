de305e49c72983c01ed2cdd2080ca89ab90b0a72
Robert Vunabandi <rvunabandi@gmail.com>
2018-06-09 20:23:20 -0700

    updated guidelines formatting: added lines

--- models/field_guidelines.js
+++ models/field_guidelines.js
@@ -1,5 +1,6 @@
 let guidelines = "\n\n";
 
+guidelines += "----------------------------------------------\n";
 guidelines += "*Expertise Input Guidelines*\n\n";
 guidelines += "- You must enter your expertise in comma separated values. ";
 guidelines += "For example, \n\n`/set_expertise computer science, sports, ice-skating`.\n\n";
@@ -7,5 +8,6 @@ guidelines += "- Each field _must_ contain only characters from the alphabet in 
 guidelines += "hyphens (-) and empty spaces ( ). If your fields contain anything else ";
 guidelines += "(no numbers allowed), your request will not be accepted. \n\n";
 guidelines += "Please follow this guideline. _Thank you for your cooperation!_";
+guidelines += "----------------------------------------------\n";
 
 module.exports = guidelines;
