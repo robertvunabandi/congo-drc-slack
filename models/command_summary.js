c006e2386d0e47f04b6aee98d13a5d91125d8a23
Robert Vunabandi <rvunabandi@gmail.com>
2018-06-09 20:54:26 -0700

    added borders around command summary

--- models/command_summary.js
+++ models/command_summary.js
@@ -1,6 +1,7 @@
 let command_summary = "";
 
 command_summary += `
+  ==============================================================================
   *Commands For Team DRC Slack App*
   - \`/set_expertise\` set your expertise so that it's known to the team.
   - \`/my_expertise\` see the set of expertise that you have set for yourself.
@@ -8,6 +9,7 @@ command_summary += `
   - \`/guideline\` see the guideline on how to set the fields you are an experts on.
   - \`/command_summary\` see this summary of commands.
   _Is this confusing?_ Feel free to DM <@U9ZAM86KC> for questions and feedback 
+  ==============================================================================
 `;
 
 module.exports = command_summary;
