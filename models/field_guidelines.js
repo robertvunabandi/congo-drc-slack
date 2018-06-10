c1090d0ce0d982d84e873971f1d414b9b1cd4c21
Robert Vunabandi <rvunabandi@gmail.com>
2018-06-09 20:18:04 -0700

    added formatting to guidelines and removed node_modules

--- models/field_guidelines.js
+++ models/field_guidelines.js
@@ -1,11 +1,11 @@
 let guidelines = "\n\n";
 
-guidelines += "Expertise Input Guidelines:\n";
+guidelines += "*Expertise Input Guidelines*\n\n";
 guidelines += "You must enter your expertise in comma separated values. ";
-guidelines += "For example, \"/set_expertise computer science, sports, ice-skating\".\n";
-guidelines += "Each field must contain only characters from the alphabet in addition to ";
+guidelines += "For example, `/set_expertise computer science, sports, ice-skating`.\n";
+guidelines += "Each field _must_ contain only characters from the alphabet in addition to ";
 guidelines += "hyphens (-) and empty spaces ( ). If your fields contain anything else ";
 guidelines += "(no numbers allowed), your request will not be accepted. Please follow ";
-guidelines += "this guideline.\n\nThank you for your cooperation!\n";
+guidelines += "this guideline.\n\n_Thank you for your cooperation!_\n";
 
 module.exports = guidelines;
