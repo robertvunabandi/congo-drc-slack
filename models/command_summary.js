a064e82fc5f82f08330839c4bc8fa97c9c8acd64
Robert Vunabandi <rvunabandi@gmail.com>
2018-06-09 21:26:22 -0700

    changes to summaries

--- models/command_summary.js
+++ models/command_summary.js
@@ -8,7 +8,8 @@ command_summary += `
   - \`/team_experts\` see who is an expert in what
   - \`/guideline\` see the guideline on how to set the fields you are an experts on.
   - \`/command_summary\` see this summary of commands.
-  _Is this confusing?_ *Feel free to DM <@U9ZAM86KC> for questions and feedback.*
+  _Is this confusing?_ *Feel free to DM <@U9ZAM86KC> for questions and feedback.* 
+  Also, the link to the Github repository is https://github.com/robertvunabandi/congo-drc-slack
   ==============================================================================
 `;
 
--- models/field_guidelines.js
+++ models/field_guidelines.js
@@ -1,18 +1,29 @@
 let guidelines = "\n\n";
 
-guidelines += "=================================================================\n";
-guidelines += "*Expertise Input Guidelines*\n\n";
-guidelines += "We would like to follow a consistent guideline for members' expertise. ";
-guidelines += "Here they are: \n";
-guidelines += "- You must enter your expertise in comma separated values. ";
-guidelines += "For example, \n\n`/set_expertise computer science, sports, ice-skating`.\n\n";
-guidelines += "- Each field _must_ contain only characters from the alphabet in addition to ";
-guidelines += "hyphens (-) and empty spaces ( ). If your fields contain anything else ";
-guidelines += "(no numbers allowed), your request will not be accepted. \n\n";
-guidelines += "Please follow this guideline. To add your expertise, simply type ";
-guidelines += "`/set_expertise` in any Slack channel, followed by a list of fields in which ";
-guidelines += "you consider yourself an expert. \n\n";
-guidelines += " _Thank you for your cooperation!_\n";
-guidelines += "=================================================================\n";
+guidelines += `
+  =================================================================
+  *Expertise Input Guidelines*
+  
+  We would like to follow a consistent guideline for members' fields of expertise, so here they are:
+  - You must enter your expertise in comma separated values. For example, 
+  
+    \`/set_expertise computer science, sports, ice-skating\`.
+  
+  - Each field _must_ contain only characters from the alphabet in addition to `;
+guidelines += `hyphens (-) and empty spaces ( ). If your fields contain anything else `;
+guidelines += `(no numbers allowed), your request will not be accepted.
 
+  Please follow this guideline. To add your expertise, simply type \`/set_expertise\``;
+guidelines += `in any Slack channel, followed by a list of fields in which `;
+guidelines += `you consider yourself an expert. 
+
+  If you have any concern or feedback, please contact <@U9ZAM86KC>.
+  
+  `;
+guidelines += "In addition, the Github repository for this project (if you are interested in ";
+guidelines += "participating in building the software for this Slack application) is";
+guidelines += `https://github.com/robertvunabandi/congo-drc-slack 
+
+  _Thank you for your cooperation!_
+  =================================================================`;
 module.exports = guidelines;
